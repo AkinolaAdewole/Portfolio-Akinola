8b03a41b93baa1ef0f1524d209ace5519fa9d76e
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-14 18:51:06 +0100

    first commit

--- src/ReUsables/NavBar.jsx
+++ src/ReUsables/NavBar.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import '../styles/navbar.css'
 
 const NavBar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Akinola Adewole</a>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light nav1 ">
+        <a className="navbar-brand  logomargin" href="#" >Akinola Adewole</a>
+
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <div className="collapse navbar-collapse navmargin" id="navbarNavDropdown">
             <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Projects</a>
-            </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home </a>
+                </li>
+                <li class="nav-item">
+                    <h3>About</h3>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Projects</a>
+                </li>
 
-            <li class="nav-item">
-                <a class="nav-link" href="#">Contact</a>
-            </li>
-            
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+                
             </ul>
         </div>
       </nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,13 @@ const Home = () => {
   return (
     <>
        <NavBar />
+
+       <div className='text-center mt-5'>
+        <h3>Hello</h3>
+        <h3> Welcome to my creative space where ideas come <br /> to life through design and innovation.</h3>
+        <h5 className='mt-3'>I design and code beautifully and I love what I do.</h5>
+
+       </div>
     </>
   )
 }
