2537cfb2a3b64fcde9d50de08b8de6e57dd15d2c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-15 15:56:03 +0100

    portfolio

--- src/ReUsables/NavBar.jsx
+++ src/ReUsables/NavBar.jsx
@@ -5,38 +5,38 @@ import '../styles/navbar.css';
 const NavBar = () => {
   return (
     <div className='bg'>
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <Link className="navbar-brand logomargin" to="/">Akinola Adewole</Link>
+          <nav className="navbar navbar-expand-lg navbar-light">
+      <Link className="navbar-brand logomargin" to="/">Akinola Adewole</Link>
+      
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/projects">Projects</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/about">About</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/projects">Projects</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,19 +5,19 @@ import akinola from '../pics/akinola.jpg'
 const Home = () => {
   return (
     <>
-      <div className='home1'>
+      <div className='home1' >
 
         <div className='passion mx-auto'>
            <div className=' '>
               <h4 className='text-center'>About me</h4>
-                <div>
+                <div className='row'>
                   <div className='flex1 pt-5'>
                     <div>
                         <h5> Welcome to my creative space where ideas come <br /> to life through design and innovation.</h5>
                         <h5 className='mt-3'>I design and code beautifully and I love what I do.</h5>
                     </div>
 
-                    <div className=''> <img className='img1' src={akinola}  /></div>
+                    <div className='delay'> <img className='img1' src={akinola}  /></div>
                  </div>
                 </div>
            </div>
