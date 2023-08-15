438726724a30f4063bef958e695fd703eab7f413
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-15 10:38:42 +0100

    portfolio

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
   
-  <NavBar />
+      <NavBar />
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import NavBar from '../ReUsables/NavBar'
+import '../styles/home.css'
 
 const Home = () => {
   return (
     <>
+      <div className='home1'>
+        <div className='passion mx-auto'>
+            <h3 className='text-center'>About me</h3>
+            <div>
+              <div>
+                  <h3> Welcome to my creative space where ideas come <br /> to life through design and innovation.</h3>
+                  <h5 className='mt-3'>I design and code beautifully and I love what I do.</h5>
+              </div>
+              <div></div>
+            </div>
+          </div>
+      </div>
 
-
-       <div className='mt-5 mx-auto passion'>
-        <h3>Hello</h3>
-        <h3> Welcome to my creative space where ideas come <br /> to life through design and innovation.</h3>
-        <h5 className='mt-3'>I design and code beautifully and I love what I do.</h5>
-
-       </div>
+      {/* <div className='bg'>
+       
+      </div> */}
+  
     </>
   )
 }
